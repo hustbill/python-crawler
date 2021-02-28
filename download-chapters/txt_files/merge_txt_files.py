fde9b2892c0aba5bc36bd351907d2343e35a31ee
hua zhang <zhahua@microsoft.com>
2021-02-28 14:39:43 -0800

    update the download-chapters for houdali memo

--- download-chapters/txt_files/merge_txt_files.py
+++ download-chapters/txt_files/merge_txt_files.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 import urllib
 import re
 import os
@@ -11,13 +11,17 @@ from urllib.parse import urlparse
 
 import re
 
+
 # Ref: https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+
+
 def atof(text):
     try:
         retval = float(text)
     except ValueError:
         retval = text
     return retval
+
 
 def natural_keys(text):
     '''
@@ -26,45 +30,53 @@ def natural_keys(text):
     (See Toothy's implementation in the comments)
     float regex comes from https://stackoverflow.com/a/12643073/190597
     '''
-    return [ atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text) ]
-
+    return [atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text)]
 
 
 def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
 
 
 def merge_txt_files():
     print("merge_txt_files: ")
     read_files = glob.glob("*.txt")
-    print(read_files)
     # sorted_files = sorted(read_files)
     # correctly sort a string with a number inside
     read_files.sort(key=natural_keys)
-    print (read_files[1:20])
-    sub_files = read_files[1:20]
-    with open("../result.txt", "wb") as outfile:
-        for f in sub_files:       
+    # print (read_files[1:20])
+    # sub_files = read_files[1:20]
+    sub_files = read_files
+    with open("result.txt", "wb") as outfile:
+        for f in sub_files:
             with open(f, "rb") as infile:
-               
-                outfile.write(f.encode('gbk'))
+                print(f)
+                # outfile.write(f.encode('gbk'))
+                # outfile.write(f)
+                base = os.path.basename(f)
+                file_name = os.path.splitext(base)[0]
+
+                section_name = ('\n\n\n### ' + file_name + " \n\n\n").encode()
+                outfile.write(section_name)
+
                 outfile.write(infile.read())
+
 
 def main():
     # sort string by number inside
-    alist=[
-    "something1",
-    "something2",
-    "something1.0",
-    "something1.25",
-    "something1.105"]
+    alist = [
+        "something1",
+        "something2",
+        "something1.0",
+        "something1.25",
+        "something1.105"]
 
     alist.sort(key=natural_keys)
     # print(alist)
     # merge sections into one file
     merge_txt_files()
+
 
 if __name__ == '__main__':
     main()
